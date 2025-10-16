93ad2782ca70147bfb148e68cfc5eaa2af86e5d6
sachin singh <rockstar15sachin@gmail.com>
2025-10-16 18:04:34 +0530

    test and functionality of single and multiple number with and without new line character

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,6 +1,19 @@
 public class StringCalculator {
     public int add(String numbers) {
         if (numbers.isEmpty()) return 0;
-        return Integer.parseInt(numbers);
+
+        //Fixed:Delimeter , or new line
+        String delimiter = "[,]|\n";
+
+        String[] token = numbers.split(delimiter);
+        int sum = 0;
+        for(String number : token){
+            if(number.isEmpty()) continue;
+            int num = Integer.parseInt(number);
+            sum += num;
+        }
+
+
+        return sum;
     }
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -7,4 +7,26 @@ public class StringCalculatorTest {
     void testEmptyStringReturnsZero() {
         assertEquals(0, new StringCalculator().add(""));
     }
+
+    @Test
+    void testSingleNumber() {
+        assertEquals(5, new StringCalculator().add("5"));
+    }
+
+    @Test
+    void testTwoNumbers() {
+        assertEquals(3, new StringCalculator().add("1,2"));
+    }
+
+    @Test
+    void testMultipleNumbersWithoutNewLine() {
+        assertEquals(15, new StringCalculator().add("1,2,3,4,5"));
+    }
+
+    @Test
+    void testMultipleNumbersWithNewLine() {
+        assertEquals(18, new StringCalculator().add("4,2\n3,4\n5"));
+    }
+
+
 }
